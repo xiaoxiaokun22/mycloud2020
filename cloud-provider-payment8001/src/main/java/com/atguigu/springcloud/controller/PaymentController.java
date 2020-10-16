893b689f93fc650e70ad7ee812b11af719e2caa8
郝国坤 <guokun.hao@mediafull.cn>
2020-10-16 18:31:49 +0800

    服务提供者discovery接口暴露服务

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,12 +1,15 @@
 package com.atguigu.springcloud.controller;
 
-import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import com.oracle.tools.packager.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,6 +18,8 @@ public class PaymentController extends BaseController{
 
    @Autowired
    private PaymentService paymentService;
+   @Autowired
+   private DiscoveryClient discoveryClient;
 
    @PostMapping("/payment/create")
    public Response create(){
@@ -25,5 +30,21 @@ public class PaymentController extends BaseController{
    public Response getPaymentById(@PathVariable("id")Long id){
        Map<String,Object> res = paymentService.getInfo(id);
        return  Response.ok(res);
+   }
+
+   @GetMapping("/payment/discovery")
+   public Object discovery(){
+       Log.info("=====所有服务=====");
+       List<String> services = discoveryClient.getServices();
+       for(String str:services){
+           Log.info("===:"+str);
+       }
+
+       Log.info("=====CLOUD-PAYMENT-SERVICE微服务下的实例=====");
+       List<ServiceInstance> serviceInstances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+       for(ServiceInstance serviceInstance:serviceInstances){
+           Log.info(serviceInstance.getUri().toString());
+       }
+       return discoveryClient;
    }
 }
