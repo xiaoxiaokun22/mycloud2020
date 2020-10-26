225dbba7811304fbf2da7276ba82f34213c42e1d
郝国坤 <guokun.hao@mediafull.cn>
2020-10-26 15:46:48 +0800

    zookeeper替代eureka做注册服务中心

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("payment/zk")
+    public String zk(){
+        return "zookeeper port:" + serverPort;
+    }
+}
