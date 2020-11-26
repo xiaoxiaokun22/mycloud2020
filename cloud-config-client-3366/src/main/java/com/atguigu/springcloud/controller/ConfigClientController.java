27ea61898be7100468eb3611734012a7f6ff9147
郝国坤 <guokun.hao@mediafull.cn>
2020-11-26 18:22:27 +0800

    bus消息中心，动态刷新

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,16 +1,11 @@
 package com.atguigu.springcloud.controller;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @RefreshScope
- * git的yml配置修改后，需要运维[手动刷新]config客户端，
- * curl -X POST "http://localhost:3355/actuator/refresh"
- */
 @RestController
 @RefreshScope
 public class ConfigClientController {
